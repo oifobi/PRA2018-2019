16fc2ce61b89d07a265f56975f51eb1ddd6243ea
s439546 <mdosl@st.wmi.amu.edu.pl>
2018-10-04 13:02:43 +0200

    oif

--- src/main/java/Pracownia/Introduction/Main.java
+++ src/main/java/Pracownia/Introduction/Main.java
@@ -1,8 +1,14 @@
 package Pracownia.Introduction;
 
+import org.apache.log4j.Logger;
+
 public class Main {
+    static Logger log = Logger.getLogger("abc");
 
     public static void main(String [ ] args) {
+
+        log.info("messeage");
+
         System.out.println("Hello World");
     }
 }
